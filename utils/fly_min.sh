c59df304fdc8d12887867ee64ea8f02593842b0b
U-CHINA\s00380291 <s00380291@S00380291.china.huawei.com>
2018-08-21 16:06:47 +0800

    adding new scripts

--- utils/fly_min.sh
+++ utils/fly_min.sh
@@ -1,5 +1,5 @@
 ./sweep.sh  ../src/booksim runfiles/ssyftree watch_out=watch_out_fattree_vc2  topology=fattree traffic=uniform num_vcs=2   > result_fattree_uniform_vc2 ; awk -f filter.awk result_fattree_uniform_vc2  | sort -g > result_fattree_uniform_vc2.1
-./sweep.sh  ../src/booksim runfiles/ssyftree watch_out=watch_out_fattree_vc4  topology=fattree traffic=uniform num_vcs=4   > result_fattree_uniform_vc4 ; awk -f filter.awk result_fattree_uniform_vc4  | sort -g > result_fattree_uniform_vc4.1
-./sweep.sh  ../src/booksim runfiles/ssyftree watch_out=watch_out_fattree_vc8  topology=fattree traffic=uniform num_vcs=8   > result_fattree_uniform_vc8 ; awk -f filter.awk result_fattree_uniform_vc8  | sort -g > result_fattree_uniform_vc8.1
-./sweep.sh  ../src/booksim runfiles/ssyftree watch_out=watch_out_fattree_vc16 topology=fattree traffic=uniform num_vcs=16  > result_fattree_uniform_vc16; awk -f filter.awk result_fattree_uniform_vc16 | sort -g > result_fattree_uniform_vc16.1
+#./sweep.sh  ../src/booksim runfiles/ssyftree watch_out=watch_out_fattree_vc4  topology=fattree traffic=uniform num_vcs=4   > result_fattree_uniform_vc4 ; awk -f filter.awk result_fattree_uniform_vc4  | sort -g > result_fattree_uniform_vc4.1
+#./sweep.sh  ../src/booksim runfiles/ssyftree watch_out=watch_out_fattree_vc8  topology=fattree traffic=uniform num_vcs=8   > result_fattree_uniform_vc8 ; awk -f filter.awk result_fattree_uniform_vc8  | sort -g > result_fattree_uniform_vc8.1
+#./sweep.sh  ../src/booksim runfiles/ssyftree watch_out=watch_out_fattree_vc16 topology=fattree traffic=uniform num_vcs=16  > result_fattree_uniform_vc16; awk -f filter.awk result_fattree_uniform_vc16 | sort -g > result_fattree_uniform_vc16.1
 
